e44a1eff8864bb97698e154059e126e84f396840
redaphid <iam@aaronherres.com>
2017-08-01 22:38:46 -0700

    react-game-kit renders something, so that's something, right?

--- src/components/Rewinder/index.js
+++ src/components/Rewinder/index.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import { Loop, Stage, World, KeyListener, AudioPlayer } from 'react-game-kit'
 export default (props) => {
-  return <h1>hi</h1>
+  return (
+    <Loop>
+      <Stage style={{ background: '#2e3235', position: 'static' }}>
+        <World onInit={this.physicsInit} onCollision={this.onCollision}>
+        </World>
+      </Stage>
+    </Loop>
+  )
 }
